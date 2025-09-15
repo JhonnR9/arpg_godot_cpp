258e4da6b5d340156b3c4a07ab868e75c84d06ad
jhonnr9 <4.jhones@gmail.com>
2025-09-15 09:37:13 -0300

    InventorySystem added

---
+++ arpg/grid_inventory.cpp
@@ -0,0 +1,46 @@
+#include "grid_inventory.h"
+#include "tools/auto_resgister.h"
+
+AUTO_REGISTER_CLASS(GridInventory)
+
+void GridInventory::_notification(int p_what) {
+	switch (p_what) {
+		case NOTIFICATION_DRAW: {
+			ERR_FAIL_NULL(bg_style_box.ptr());
+
+			const RID ci = get_canvas_item();
+			const Vector2 control_size = get_size();
+			const Rect2 rect(Point2(0, 0), Point2(control_size.x, control_size.y));
+
+			bg_style_box->draw(ci, rect);
+
+			break;
+		}
+
+		default:;
+	}
+}
+
+Ref<StyleBox> GridInventory::get_bg_style_box() const {
+	return bg_style_box;
+}
+void GridInventory::set_bg_style_box(const Ref<StyleBox> &p_bg_style_box) {
+	this->bg_style_box = p_bg_style_box;
+}
+Ref<StyleBox> GridInventory::get_item_frame_style_box() const {
+	return item_frame_style_box;
+}
+void GridInventory::set_item_frame_style_box(const Ref<StyleBox> &p_item_frame_style_box) {
+	this->item_frame_style_box = p_item_frame_style_box;
+}
+
+void GridInventory::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("get_bg_style_box"), &GridInventory::get_bg_style_box);
+	ClassDB::bind_method(D_METHOD("set_bg_style_box", "bg_style_box"), &GridInventory::set_bg_style_box);
+
+	ClassDB::bind_method(D_METHOD("get_item_frame_style_box"), &GridInventory::get_item_frame_style_box);
+	ClassDB::bind_method(D_METHOD("set_item_frame_style_box", "item_frame_style_box"), &GridInventory::set_item_frame_style_box);
+
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "bg_style_box", PROPERTY_HINT_RESOURCE_TYPE, "StyleBox"), "set_bg_style_box", "get_bg_style_box");
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "item_frame_style_box", PROPERTY_HINT_RESOURCE_TYPE, "StyleBox"), "set_item_frame_style_box", "get_item_frame_style_box");
+}
---
+++ arpg/grid_inventory.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "godot_cpp/classes/control.hpp"
+#include "godot_cpp/classes/style_box.hpp"
+
+using namespace godot;
+
+class GridInventory final : public Control {
+	GDCLASS(GridInventory, Control)
+
+	Ref<StyleBox> bg_style_box;
+	Ref<StyleBox> item_frame_style_box;
+	int rows = 5;
+	int columns = 5;
+	Vector2 slot_size = Vector2(64, 64);
+
+protected:
+	static void _bind_methods();
+	void _notification(int p_what);
+
+public:
+	GridInventory() = default;
+
+	Ref<StyleBox> get_bg_style_box() const;
+	void set_bg_style_box(const Ref<StyleBox> &p_bg_style_box);
+
+	Ref<StyleBox> get_item_frame_style_box() const;
+	void set_item_frame_style_box(const Ref<StyleBox> &p_item_frame_style_box);
+};
