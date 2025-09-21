989379f07507e2ea01e2fb12a8b8a280f0ea8c75
jhonnr9 <4.jhones@gmail.com>
2025-09-21 15:29:13 -0300

    same

---
+++ arpg/slot_panel.cpp
@@ -0,0 +1,113 @@
+
+#include "slot_panel.h"
+#include "grid_inventory.h"
+#include "tools/auto_resgister.h"
+using namespace godot;
+
+AUTO_REGISTER_INTERNAL_CLASS(SlotPanel)
+
+void SlotPanel::_bind_methods() {
+}
+void SlotPanel::_notification(int p_what) {
+	switch (p_what) {
+		case NOTIFICATION_ENTER_TREE: {
+			if (auto *node = get_parent()) {
+				inventory = cast_to<GridInventory>(node);
+			}
+			break;
+		}
+		case NOTIFICATION_EXIT_TREE: {
+			inventory = nullptr;
+			memdelete(preview);
+			preview = nullptr;
+			break;
+		}
+		default:;
+	}
+}
+
+SlotPanel::SlotPanel() :
+		inventory(nullptr), preview(nullptr) {
+	preview = memnew(TextureRect);
+}
+
+Variant SlotPanel::_get_drag_data(const Vector2 &p_at_position) {
+	if (!inventory) {
+		preview->set_texture(Ref<Texture>{});
+		preview->hide();
+		ERR_PRINT("Inventory can't be null");
+		return Variant();
+	}
+	if (!preview) {
+		preview = memnew(TextureRect);
+	}
+
+	if (auto *slot = inventory->cells.getptr(key_in_grid)) {
+		if (slot->item.is_valid()) {
+			preview->set_texture(slot->item->get_icon());
+			preview->set_expand_mode(TextureRect::EXPAND_IGNORE_SIZE);
+			preview->set_stretch_mode(TextureRect::STRETCH_KEEP_ASPECT_CENTERED);
+			preview->set_size(slot->rect.size);
+			preview->set_pivot_offset(slot->rect.get_size() / 2);
+			preview->show();
+			set_drag_preview(preview);
+			preview = nullptr; //now godot can delete then
+
+			Ref<ItemView> item = slot->item;
+			inventory->_clear_slot(*slot);
+
+			return item;
+		}
+	}
+
+	preview->set_texture(Ref<Texture>{});
+	preview->hide();
+	return Variant();
+}
+
+bool SlotPanel::_can_drop_data(const Vector2 &p_at_position, const Variant &p_data) const {
+	if (!inventory) {
+		return false;
+	}
+
+	if (const auto *slot = inventory->cells.getptr(inventory->hovered_slot_key)) {
+		if (slot->item.is_null()) {
+			return true;
+		}
+
+		const Ref<ItemView> item = p_data;
+		if (item.is_null()) {
+			return false;
+		}
+
+		if (slot->item->get_id() == item->get_id()) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void SlotPanel::_drop_data(const Vector2 &p_at_position, const Variant &p_data) {
+	if (!inventory) {
+		ERR_PRINT("Inventory é nullptr em _drop_data");
+		return;
+	}
+
+	if (const auto *slot = inventory->cells.getptr(inventory->hovered_slot_key)) {
+		const Ref<ItemView> item = p_data;
+		if (item.is_valid()) {
+			if (slot->item.is_null()) {
+				const Point2i hovered_pos{
+					inventory->_get_col_from_key(inventory->hovered_slot_key),
+					inventory->_get_row_from_key(inventory->hovered_slot_key)
+				};
+
+				inventory->add_item_at(item, hovered_pos);
+			}
+		}
+	}
+	if (preview) {
+		preview->set_texture(Ref<Texture>());
+		preview->hide();
+	}
+}
---
+++ arpg/slot_panel.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "godot_cpp/classes/panel.hpp"
+#include "grid_inventory.h"
+
+namespace godot {
+class TextureRect;
+}
+namespace godot {
+
+class GridInventory;
+class SlotPanel final : public Panel {
+	GDCLASS(SlotPanel, Panel)
+
+	GridInventory *inventory;
+	TextureRect *preview;
+	int64_t key_in_grid = INVALID_KEY;
+
+protected:
+	static void _bind_methods();
+	void _notification(int p_what);
+	public:
+	SlotPanel();
+
+	Variant _get_drag_data(const Vector2 &p_at_position) override;
+	bool _can_drop_data(const Vector2 &p_at_position, const Variant &p_data) const override;
+	void _drop_data(const Vector2 &p_at_position, const Variant &p_data) override;
+public:
+	void set_key_in_grid(const int64_t p_key_in_grid) {
+		key_in_grid = p_key_in_grid;
+	}
+};
+} //namespace godot
